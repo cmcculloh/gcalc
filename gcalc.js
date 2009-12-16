3450a6d22c5bfd2ce1d1e293d99533b20b963357
cmcculloh <cmcculloh@gmail.com>
2009-12-16 11:04:29 -0700

    Abstracted for use in Chrome Extension

--- gcalc.js
+++ gcalc.js
@@ -1,42 +1,29 @@
-// Ubiquity Google calculator script
+// Chrome Google calculator script
 // by Zach Carter http://zaa.ch
-// MPL, MIT licensed 2008
+// forked by cmcculloh for Status-bar Calculator http://statusbarcalculator.com
+// MPL, MIT licensed 2009
+//     This can be used in a Google Chrome extension (or on any web page running jQuery) 
+//     to calculate an expression using Google's calculator
 
 (function(){
-
-function scrapeResult(data) {
-    if(data.match(/<img src=\/images\/calc_img.gif width=40 height=30 alt="">/)){
-      return data.match(/<h2 class=r><font size=\+1><b>(.+)<\/b><\/h2>/)[1];
-    }else {
-      return 'Unrecognized expression.'}
-}
-
-CmdUtils.CreateCommand({
-  name: "gcalc",
-
-  icon: 'http://www.google.com/images/calc_img.gif',
-  homepage: "http://www.google.com/help/calculator.html",
-  description: "Use Google Search's calculator capibilities.",
-  author: { name: "Zach Carter", email: "zack.carter@gmail.com", homepage: "http://zaa.ch/"},
-  
-  takes: {expression: noun_arb_text},
-
-  preview: function(pblock, statusText) {
-    var pb = pblock;
-    pblock.innerHTML = 'Enter an expression to evaluate.';
-    if(statusText.text.length > 1){
-      jQuery.get("http://www.google.com/search",{q: statusText.text}, function(data){pb.innerHTML = scrapeResult(data);});
-      pblock.innerHTML = 'Loading result...';
+    function gcalc(expression) {
+        if(expression.length > 1){
+            $.get("http://www.google.com/search",
+                {
+                    q: expression
+                }, 
+                function(data){
+                    return scrapeResult(data);
+                }
+            );
+        }
     }
-  },
-  
-  execute: function(statusText) {
-    if(statusText.text.length < 1) {
-      displayMessage("Please enter an expression to evaulate.");
-      return;
-    }
-    Utils.openUrlInBrowser('http://www.google.com/search?q=' + encodeURIComponent(statusText.text));  
-  }
-});
 
+    function scrapeResult(data) {
+        if(data.match(/<img src=\/images\/calc_img.gif width=40 height=30 alt="">/)){
+            return data.match(/<h2 class=r><font size=\+1><b>(.+)<\/b><\/h2>/)[1];
+        }else{
+            return 'ERROR';
+        }
+    }
 })();
