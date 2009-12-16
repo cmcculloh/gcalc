ce00a73ae8c38744f4320f6448cdb8505cf2ab46
cmcculloh <cmcculloh@gmail.com>
2009-12-16 13:11:14 -0700

    corrected some syntax errors, made it a jQuery plugin since it relies on jQuery anyways. Made the return a console.log() because returning will do nothing since it is asynchronous...

--- gcalc.js
+++ gcalc.js
@@ -5,25 +5,27 @@
 //     This can be used in a Google Chrome extension (or on any web page running jQuery) 
 //     to calculate an expression using Google's calculator
 
-(function(){
-    function gcalc(expression) {
+(function($){
+    $.fn.gcalc = function(expression) {
         if(expression.length > 1){
             $.get("http://www.google.com/search",
                 {
                     q: expression
                 }, 
                 function(data){
-                    return scrapeResult(data);
+                    console.log($().scrapeResult(data));
                 }
             );
         }
     }
 
-    function scrapeResult(data) {
+    $.fn.scrapeResult = function(data) {
         if(data.match(/<img src=\/images\/calc_img.gif width=40 height=30 alt="">/)){
             return data.match(/<h2 class=r><font size=\+1><b>(.+)<\/b><\/h2>/)[1];
         }else{
             return 'ERROR';
         }
     }
-})();
+})(jQuery);
+
+//$().gcalc("1+1');
